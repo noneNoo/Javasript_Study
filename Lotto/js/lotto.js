14f7b2c8662a09da1cd4af44e40b28f4b5a21e7a
noneno <noneno@kakao.com>
2020-12-10 21:56:47 +0900

    lotto.js 랜덤한 7개의 번호 추첨하는 기능 구현
    후보군 배열에서 랜덤한 수를 추출하여 splice로 잘라내고 새로운 배열에
    push하여, 45개의 숫자를 모두 랜덤하게 섞은 새 배열을 만들어냈다.
    
    shuffle 배열에서 6개의 번호와 보너스 번호를 추출해냈다.

--- Lotto/js/lotto.js
+++ Lotto/js/lotto.js
@@ -2,6 +2,22 @@
 const candidate = Array(45)
   // fill메서드로 배열들을 undefind 로 만든다
   .fill()
+  // map메서드로 배열들에 index값을 부여한다
   .map((arr, index) => index + 1);
 
-console.log(candidate);
+const shuffle = [];
+
+// while문을 돌 때마다 candidate 에서 배열이 하나씩 빠지고
+// shuffle에 배열이 push된다
+while (candidate.length > 0) {
+  // const value = candidate.splice(Math.floor(Math.random() * candidate.length),1);
+  // 후보군 중 랜덤한 숫자 임의로 뽑기
+  const random = Math.floor(Math.random() * candidate.length);
+  // splice 메서드를 이용하여 배열에서 랜덤한 값을 1개 잘라온다
+  const spliceArray = candidate.splice(random, 1);
+  const value = spliceArray[0];
+  shuffle.push(value);
+}
+
+const winBalls = shuffle.slice(0, 6);
+const bonusBall = shuffle[6];
